563b6ee4d5122af797601b0afe8548cf7aa43d3d
ferdynandcv <fernandocantoralv@gmail.com>
2024-05-22 14:46:53 -0600

    solo funciones

--- lexico.py
+++ lexico.py
@@ -1,5 +1,5 @@
 def tokenize(command):
     parts = command.split('(')
     command_name = parts[0].strip()
-    params = parts[1].strip()[:-1].split(',')
+    params = [param.strip() for param in parts[1].strip()[:-1].split(',')]  # Agregar strip() para eliminar espacios en blanco alrededor de los parámetros
     return command_name, params
